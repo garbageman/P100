0d838dc964715628dd1f885d3d84f6224025cb2e
garbageman <dnagle@umd.edu>
2017-01-18 00:57:30 -0500

    Removed canonical

--- P1/test/canonical.py
+++
@@ -1,7 +0,0 @@
-#-------------------------------------------------------------------------------
-# P1 - Input and Output
-# @author Damien Nagle
-# ------------------------------------------------------------------------------
-
-# Write a program that prints "I love python!" to the console
-print "I love python!"
